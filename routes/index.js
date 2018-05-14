842aa175415540024feecf796a05ce60f94aca38
Vbilot <valentin.bilot@gmail.com>
2018-05-14 11:42:21 +0200

    mise en place du squelette des routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user',(req,res)=>{
+  // aller chercher data dans la base de donnée
+  data={
+    /*
+    {
+      user1:coordonnée
+      user2:coordonnée
+    }
+   */
+  }
+  res.send(data);
+});
+
+router.get('/user:id',(req,res)=>{
+  // aller chercher data dans la base de donnée
+  data={
+    /*
+    {
+      user1:coordonnée
+    }
+   */
+  }
+  res.send(data);
+
+});
+
+router.post('/user',(req,res)=>{
+  // rajouter ce user à la base de données
+});
+
+router.update('/user:id',(req,res)=>{
+  // mettre à jour la position de cet id
+});
+
+router.delete('/user',(req,res)=>{
+  //supprimer ce user de la DB
+});
+
 module.exports = router;
