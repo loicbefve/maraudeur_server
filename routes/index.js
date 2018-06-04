b8fa2674d115d5d55bcb27cf41bf80576f4422bc
valentinbilot <valentin.bilot@gmail.com>
2018-06-04 11:50:54 +0200

    ajout du json dans le get user

--- routes/index.js
+++ routes/index.js
@@ -146,8 +146,9 @@ router.get('/user',(req,res)=>{
     for (let row of datares.rows) {
       console.log(JSON.stringify(row));
       str=str+JSON.stringify(row);
+      res.write(JSON.stringify(row));
     }
-    res.render('index', {title:str});
+    res.send()
     client.end();
   });
   console.log("hey");
